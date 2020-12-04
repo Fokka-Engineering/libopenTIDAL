c387015ea89f716fca6533db9f16ad609bd0eb19
DerNuntius <hugo.melder@gmail.com>
2020-12-04 13:31:56 +0100

    GCC Linux Warning #3

--- src/parse/json.c
+++ src/parse/json.c
@@ -28,7 +28,7 @@
 cJSON *json_parse(char * input)
 {
   cJSON *input_json = cJSON_Parse(input);
-  int status = 0;
+  //int status = 0;
   if (input_json == NULL)
   {
       const char *error_ptr = cJSON_GetErrorPtr();
@@ -37,7 +37,7 @@ cJSON *json_parse(char * input)
           /*JSON Input empty*/
           fprintf(stderr, "[Request] JSON Input empty\n");
       }
-      status = 0;
+      //status = 0;
   }
   return input_json;
 }
