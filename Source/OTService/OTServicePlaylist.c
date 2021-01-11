1adb05ff075cd292febc075b0517cb23cc02f596
DerNuntius <hugo.melder@gmail.com>
2021-01-11 14:09:34 +0100

    Implemented all playlist service endpoints. Manipulation works.

--- Source/OTService/OTServicePlaylist.c
+++ Source/OTService/OTServicePlaylist.c
@@ -43,6 +43,9 @@ OTServiceCreatePlaylist (struct OTSessionContainer *session, char *title, char *
     char *encDescription = NULL;
     enum OTHttpTypes reqType = POST;
 
+    if (session->restrictedMode)
+        return content;
+
     /* UrlEncode strings. */
     encTitle = OTUrlEncode (title);
     encDescription = OTUrlEncode (description);
@@ -101,7 +104,8 @@ OTServiceGetPlaylistEntityTag (struct OTSessionContainer *session, const char *c
             char *tmp = OTHttpParseHeader (http.response, "etag");
             if (tmp)
                 {
-                    if (tmp[strlen (tmp) - 1] == '\x20')
+                    /* Remove whitespace after value (libcurl does not like this whitespace). */
+                    if (tmp[strlen (tmp) - 1] != '"')
                         tmp[strlen (tmp) - 1] = '\0';
                     value = strdup (tmp);
                 }
@@ -111,4 +115,192 @@ end:
     free (http.parameter);
     free (http.response);
     return value;
+}
+
+enum OTStatus
+OTServiceDeletePlaylistItem (struct OTSessionContainer *session, const char *const id,
+                             const int index, void *threadHandle)
+{
+    int isException = 0;
+    struct OTHttpContainer http;
+    char *entityTag;
+    enum OTHttpTypes reqType = DELETE;
+    enum OTStatus status = UNKNOWN;
+
+    if (session->restrictedMode)
+        return status;
+
+    entityTag = OTServiceGetPlaylistEntityTag (session, id, threadHandle);
+    if (!entityTag)
+        {
+            isException = 1;
+            status = PRECONDITION_FAILED;
+            goto end;
+        }
+    /* Initialise values in structure. */
+    OTHttpContainerInit (&http);
+    http.type = &reqType;
+    http.isDummy = 1;
+    OTConcatenateString (&http.endpoint, "/v1/playlists/%s/items/%d", id, index);
+    OTConcatenateString (&http.parameter, "countryCode=%s", session->countryCode);
+    OTConcatenateString (&http.entityTagHeader, "if-none-match: %s", entityTag);
+    if (!http.parameter || !http.endpoint || !http.entityTagHeader)
+        {
+            isException = 1;
+            goto end;
+        }
+
+    status = OTServiceRequestSilent (session, &http, threadHandle);
+end:
+    free (http.endpoint);
+    free (http.parameter);
+    free (http.entityTagHeader);
+    free (entityTag);
+    return status;
+}
+
+enum OTStatus
+OTServiceMovePlaylistItem (struct OTSessionContainer *session, const char *const id,
+                           const int index, const int toIndex, void *threadHandle)
+{
+    int isException = 0;
+    struct OTHttpContainer http;
+    char *entityTag;
+    enum OTHttpTypes reqType = POST;
+    enum OTStatus status = UNKNOWN;
+
+    if (session->restrictedMode)
+        return status;
+
+    entityTag = OTServiceGetPlaylistEntityTag (session, id, threadHandle);
+    if (!entityTag)
+        {
+            isException = 1;
+            status = PRECONDITION_FAILED;
+            goto end;
+        }
+    /* Initialise values in structure. */
+    OTHttpContainerInit (&http);
+    http.type = &reqType;
+    http.isDummy = 1;
+    OTConcatenateString (&http.endpoint, "/v1/playlists/%s/items/%d", id, index);
+    OTConcatenateString (&http.parameter, "countryCode=%s", session->countryCode);
+    OTConcatenateString (&http.postData, "toIndex=%d", toIndex);
+    OTConcatenateString (&http.entityTagHeader, "if-none-match: %s", entityTag);
+    if (!http.parameter || !http.endpoint || !http.postData || !http.entityTagHeader)
+        {
+            isException = 1;
+            goto end;
+        }
+
+    status = OTServiceRequestSilent (session, &http, threadHandle);
+end:
+    free (http.endpoint);
+    free (http.parameter);
+    free (http.postData);
+    free (http.entityTagHeader);
+    free (entityTag);
+    return status;
+}
+
+enum OTStatus
+OTServiceAddPlaylistItem (struct OTSessionContainer *session, const char *const id,
+                          const char *itemId, const char *onArtifactNotFound, const char *onDupes,
+                          void *threadHandle)
+{
+    int isException = 0;
+    struct OTHttpContainer http;
+    char *entityTag;
+    enum OTHttpTypes reqType = POST;
+    enum OTStatus status = UNKNOWN;
+
+    if (session->restrictedMode)
+        return status;
+
+    entityTag = OTServiceGetPlaylistEntityTag (session, id, threadHandle);
+    if (!entityTag)
+        {
+            isException = 1;
+            status = PRECONDITION_FAILED;
+            goto end;
+        }
+    /* Initialise values in structure. */
+    OTHttpContainerInit (&http);
+    http.type = &reqType;
+    http.isDummy = 1;
+    OTConcatenateString (&http.endpoint, "/v1/playlists/%s/items", id);
+    OTConcatenateString (&http.parameter, "countryCode=%s", session->countryCode);
+    OTConcatenateString (&http.postData, "itemIds=%s&onArtifactNotFound=%s&onDupes=%s", itemId,
+                         onArtifactNotFound, onDupes);
+    OTConcatenateString (&http.entityTagHeader, "if-none-match: %s", entityTag);
+    if (!http.parameter || !http.endpoint || !http.postData || !http.entityTagHeader)
+        {
+            isException = 1;
+            goto end;
+        }
+
+    status = OTServiceRequestSilent (session, &http, threadHandle);
+end:
+    free (http.endpoint);
+    free (http.parameter);
+    free (http.postData);
+    free (http.entityTagHeader);
+    free (entityTag);
+    return status;
+}
+
+enum OTStatus
+OTServiceAddPlaylistItems (struct OTSessionContainer *session, const char *const id,
+                           const char **itemIds, const int size, const char *onArtifactNotFound,
+                           const char *onDupes, void *threadHandle)
+{
+    int isException = 0;
+    struct OTHttpContainer http;
+    char *entityTag;
+    char *arrayString;
+    enum OTHttpTypes reqType = POST;
+    enum OTStatus status = UNKNOWN;
+
+    if (session->restrictedMode)
+        return status;
+
+    entityTag = OTServiceGetPlaylistEntityTag (session, id, threadHandle);
+    if (!entityTag)
+        {
+            isException = 1;
+            status = PRECONDITION_FAILED;
+            goto end;
+        }
+    /* Initialise values in structure. */
+    OTHttpContainerInit (&http);
+    http.type = &reqType;
+    http.isDummy = 1;
+
+    OTArrayToString (&arrayString, itemIds, size);
+    if (!arrayString)
+        {
+            isException = 1;
+            goto end;
+        }
+
+    OTConcatenateString (&http.endpoint, "/v1/playlists/%s/items", id);
+    OTConcatenateString (&http.parameter, "countryCode=%s", session->countryCode);
+    OTConcatenateString (&http.postData, "itemIds=%s&onArtifactNotFound=%s&onDupes=%s", arrayString,
+                         onArtifactNotFound, onDupes);
+    OTConcatenateString (&http.entityTagHeader, "if-none-match: %s", entityTag);
+    if (!http.parameter || !http.endpoint || !http.postData || !http.entityTagHeader)
+        {
+            isException = 1;
+            goto end;
+        }
+
+    status = OTServiceRequestSilent (session, &http, threadHandle);
+end:
+    free (http.endpoint);
+    free (http.parameter);
+    free (http.postData);
+    free (http.entityTagHeader);
+    free (entityTag);
+    free (arrayString);
+    return status;
 }
