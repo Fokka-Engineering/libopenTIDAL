ef7ae1f1dad3549e94ba9f7387691d92650782af
DerNuntius <hugo.melder@gmail.com>
2021-01-22 07:54:52 +0100

    Remove: Makefile

---
+++ CMakeFiles/openTIDAL.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/openTIDAL.dir/Source/OTAlloc.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTBase64.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTDealloc.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTHttp.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTHttpParse.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTJson.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTPersistent.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTService/OTService.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTService/OTServiceAuth.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTService/OTServiceFavorites.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTService/OTServiceFeed.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTService/OTServicePlaylist.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTService/OTServiceStd.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTSession.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTSessionRefresh.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTString.c.o"
+  "CMakeFiles/openTIDAL.dir/Source/OTUrlEncode.c.o"
+  "libopenTIDAL.dylib"
+  "libopenTIDAL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/openTIDAL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
